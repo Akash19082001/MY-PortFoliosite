158d12e70ddde878db0c54c343de32cceea73125
Akash19082001 <akash19m08y2001@gmail.com>
2024-08-27 19:40:23 +0530

    fist commit

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -41,34 +41,19 @@ export default function Resume() {
                        <div className='block md:hidden'>
                      <div className="flex-col items-center justify-center text-slate-50">
                         <div className=" w-[17rem] max-w-[200vw] flex flex-col gap-4 relative py-6 overflow-hidden">
-                            <div className="loop-slider" style={{ "--duration": "4300ms", "--direction": "normal" }}>
-                            <div className="inner flex animate-loop" style={{ animationDirection: "var(--direction)", animationDuration: "var(--duration)" }}>
-                            {/* <div className="tag"># JavaScript</div>
-                            <div className="tag"># Express</div>
-                            <div className="tag"># Node.js</div>
-                            <div className="tag"># mongoDb</div>
-                            <div className="tag"># UI/UX</div> */}
-                                {/* Duplicated content */}
-                                {/* <div className="tag"># JavaScript</div>
-                                <div className="tag"># Express</div>
-                                <div className="tag"># Node.js</div>
-                                <div className="tag"># mongoDb</div>
-                                <div className="tag"># UI/UX</div> */}
-                            </div>
-                            </div>
                             <div className="loop-slider" style={{ "--duration": "20000ms", "--direction": "reverse" }}>
                             <div className="inner flex animate-loop" style={{ animationDirection: "var(--direction)", animationDuration: "var(--duration)" }}>
                             <div className="tag"># Express</div>
                             <div className="tag"># Node.js</div>
                             <div className="tag"># mongoDb</div>
                             <div className="tag"># Tailwind</div>
-                            <div className="tag"># Reactjs</div> 
+                            <div className="tag"># React.js</div> 
                                 {/* Duplicated content */}
-                                <div className="tag"># Express</div>
-                            <div className="tag"># Node.js</div>
-                            <div className="tag"># mongoDb</div>
+                                <div className="tag"># java</div>
+                            <div className="tag"># postman</div>
+                            <div className="tag"># JavaScript</div>
                             <div className="tag"># Tailwind</div>
-                            <div className="tag"># Reactjs</div>
+                            <div className="tag"># Aws</div>
                             </div>
                             </div>
                             
